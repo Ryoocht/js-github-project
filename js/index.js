92999c72231cd0ba0e54ddba494d29255ca5c21c
Ryochi <ryo1.t.zubu@gmail.com>
2021-07-23 11:00:58 +1000

    Done with assignment

--- js/index.js
+++ js/index.js
@@ -1,55 +1,53 @@
-const searchForm = document.querySelector("#github-form");
-const searchField = document.querySelector("#search");
-const userList = document.querySelector("#user-list");
-const reposList = document.querySelector("#repos-list");
+const searchForm = document.querySelector('#github-form')
+const searchField = document.getElementById('search');
+const userList = document.querySelector('#user-list')
+const reposList = document.querySelector('#repos-list')
 
-searchForm.addEventListener("submit", e => {
+searchForm.addEventListener('submit',(e)=>{
     e.preventDefault();
-    if(searchField.value !== ""){
-    fetchUsers(searchField.value);
-    };
+    fetch(`https://api.github.com/search/users?q=${searchField.value}`)
+    .then(resp => resp.json())
+    .then(userData => {
+        userInfo(userData); 
+    })
+    .catch((err)=>{
+        console.log(err)
+    });
 });
 
-function fetchUsers(users){
-    const url = `https://api.github.com/search/users?q=${users}`;
-
-    fetch(url, {headers:{'Accept': 'application/vnd.github.v3+json'}})
-    .then(resp => resp.json)
-    .then(resultObj => {
-        resultObj.items.forEach(user => renderUser(user));
-    })
-    .catch(err => console.error(err));
-}
-
-function renderUser(user){
-    const li = document.createElement("li");
-    li.innerHTML = `<strong>${user.login}:</strong> Github: ${user.url}`;
-    li.style.cursor = "pointer";
-    const avatar =document.createElement("img");
-    avatar.src = user.avatar_url;
-    avatar.style.width = "20px";
-    li.appendChild(avatar);
-    userList.appendChild(li);
-    li.addEventListener("click", fetchRepos(user.login));
-}
-
-function fetchRepos(userlogin){
-    const url = `https://api.github.com/users/${userlogin}/repos`;
-    fetch(url, {headers:{'Accept': 'application/vnd.github.v3+json'}})
-    .then(resp => resp.json)
-    .then(resultObj => {
-        resultObj.forEach(repo => renderRepo(repo));
-    })
-    .catch(err => console.error(err));
-}
-
-function renderRepo(repo){
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    a.href = repo.html_url;
-    a.target = "_blank";
-    a.innerHTML = "Link";
-    li.innerHTML = repo.name + " - ";
-    li.appendChild(a);
-    reposList.appendChild(li);
+function userInfo(userData){
+    userList.innerHTML = ''
+    reposList.innerHTML = ''
+    for (let i = 0; i < userData.items.length; i++) {
+    let avatar = userData.items[i].avatar_url;
+    let name = userData.items[i].login;
+    let li = document.createElement('li')
+    let libutton = document.createElement('li')
+    libutton.classList.add('flex')
+    li.innerHTML = `<div>
+    <img src="${avatar}" alt="avatar"><br>
+    <h1>${name}</h1>
+    </div>`;
+    libutton.innerHTML=`<div>
+    <a href="${userData.items[i].html_url}">see profile</a><br>
+    <button class="btn">User's Repos</button>
+  </div>`
+    userList.appendChild(li)
+    userList.appendChild(libutton)
+    let btn = document.querySelectorAll('.btn')
+    for (let j = 0; j < btn.length; j++) {
+         btn[j].addEventListener('click', ()=>{
+            reposList.innerHTML='';
+            fetch(`${userData.items[j].repos_url}`)
+            .then(response => response.json())
+            .then(data => {
+                for(let j=0; j<data.length; j++){
+                    reposList.insertAdjacentHTML('beforeend',`<li>${data[j].full_name}</li>`);
+                } 
+            }).catch((err)=>{
+                console.log(err)
+            }); 
+    }) 
+    }
+    }
 }
